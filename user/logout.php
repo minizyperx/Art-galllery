679c09a815191342304e6dcafba89f01c9680eb2
Sumit Raj <sumit.like.raj@gmail.com>
2025-10-07 02:54:02 +0530

    project

--- user/logout.php
+++ user/logout.php
@@ -1,13 +1,36 @@
 <?php
-// logout.php
-
-// Start the session
 session_start();
 
-// Destroy the session
-session_destroy();
+class SessionManager {
+    // Logout function
+    public function logout($redirectPage = '../index.php') {
+        // Unset all session variables
+        $_SESSION = [];
 
-// Redirect to the login page
-header("Location: ../index.php");
-exit();
+        // Delete session cookie (if exists)
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+
+        // Destroy the session completely
+        session_destroy();
+
+        // Redirect to login or homepage
+        $this->redirect($redirectPage);
+    }
+
+    // Private redirect helper
+    private function redirect($page) {
+        header("Location: " . $page);
+        exit();
+    }
+}
+
+// Create object and call logout
+$session = new SessionManager();
+$session->logout('../index.php');
 ?>
